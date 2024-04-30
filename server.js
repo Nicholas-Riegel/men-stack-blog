240c4d50a34752f48d163dbf26153a307ab3785e
Nicholas <nicholaspriegel@gmail.com>
2024-04-30 09:51:15 -0400

    created new post page and route

--- server.js
+++ server.js
@@ -10,10 +10,26 @@ mongoose.connection.on('connected', ()=>{
     console.log(`Connected to MongoDB on ${mongoose.connection.name}`);
 })
 
+// GET	/blog	Read	index	Display a list of all posts.
 
-app.get('/', (req, res)=>{
+app.get('/blog', (req, res)=>{
     res.render('index.ejs')
 })
+
+// GET	/blog/new	Read	new	Show a form to add a new post.
+app.get('/blog/new', (req, res)=>{
+    res.render('new.ejs')
+})
+
+// POST	/blog	Create	create	Add a new post to the list.
+
+// GET	/blog/:id	Read	show	Display a specific post’s details.
+
+// GET	/blog/:id/edit	Read	edit	Show a form to edit an existing post’s details.
+
+// PUT	/blog/:id	Update	update	Update a specific post’s details.
+
+// DELETE	/blog/:id	Delete	delete	Remove a specific post from the list.
 
 app.listen(port, ()=>{
     console.log(`App listening on port ${port}`);

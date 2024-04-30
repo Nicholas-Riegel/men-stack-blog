d2d3d647dff56032b110d3ff5524bf0f3d5bdab2
Nicholas <nicholaspriegel@gmail.com>
2024-04-30 10:57:22 -0400

    added delete

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const port = 3000
 const Post = require('./models/post.js')
+const morgan = require('morgan')
 
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
 
 mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', ()=>{
@@ -21,11 +24,13 @@ app.get('/blog', async (req, res)=>{
 })
 
 // GET	/blog/new	Read	new	Show a form to add a new post.
+
 app.get('/blog/new', (req, res)=>{
     res.render('new.ejs')
 })
 
 // POST	/blog	Create	create	Add a new post to the list.
+
 app.post('/blog', async (req, res)=>{
     await Post.create({
         title: req.body['post-title'],
@@ -36,11 +41,22 @@ app.post('/blog', async (req, res)=>{
 
 // GET	/blog/:id	Read	show	Display a specific post’s details.
 
+app.get('/blog/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('show.ejs', {post})
+})
+
 // GET	/blog/:id/edit	Read	edit	Show a form to edit an existing post’s details.
 
 // PUT	/blog/:id	Update	update	Update a specific post’s details.
 
 // DELETE	/blog/:id	Delete	delete	Remove a specific post from the list.
+app.delete('/blog/:id', async (req, res)=>{
+    await Post.findByIdAndDelete(req.params.id)
+    res.redirect('/blog')
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`App listening on port ${port}`);
